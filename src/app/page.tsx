5eb911376c328b343255c31c1fb946ddba6b9854
Marko Piers <markopiers8@gmail.com>
2025-09-01 19:55:45 +0600

    add footer component and install lucide-react for icons
    Co-authored-by: Tasin Williamson <125305287+tasin2610@users.noreply.github.com>

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,9 @@
+import { Button } from "@/components/ui/button";
+import Footer from "@/components/footer";
 
 export default function HomePage(){
   return <main>
-    
+    <Button>Test</Button>
+    <Footer />
   </main>
 }

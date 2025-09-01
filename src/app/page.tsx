24713d92ed4e52dcff53e97b3e7bf7b95854a3a7
Tasin Williamson <125305287+tasin2610@users.noreply.github.com>
2025-09-01 19:58:32 +0600

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,6 @@
 import { Button } from "@/components/ui/button";
 import Footer from "@/components/footer";
 import StatsSection from "@/components/statsSection";
-=======
 
 export default function HomePage(){
   return <main>

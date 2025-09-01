6bd9c78d359c1fa0ca2d23c520bdeb1e4c8e6eb0
Kalida Tony <kalidatony@gmail.com>
2025-09-01 10:28:47 -0500

    add MeetSentioPulseSection component with dark glassmorphism cards and gradient background

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 import { Button } from "@/components/ui/button";
 import Footer from "@/components/footer";
 import StatsSection from "@/components/statsSection";
+import MeetSentioPulseSection from "@/components/meetSentioPulseSection";
 
 export default function HomePage(){
   return <main>
     <Button>Test</Button>
+    <MeetSentioPulseSection />
     <StatsSection />
 
     <Footer />

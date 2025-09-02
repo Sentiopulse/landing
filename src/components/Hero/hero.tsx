0dd7c5aff9c77be0c8baa1fb056cdb819281222c
Ethan Wallace <alu.pesto.en@gmail.com>
2025-09-02 08:23:05 -0600

    Add hero section image and refactor Hero and Navbar components for improved layout and styling

--- src/components/Hero/hero.tsx
+++ src/components/Hero/hero.tsx
@@ -5,13 +5,23 @@ import { HeroFooterInfo } from "@/lib/mockData";
 
 export default function HeroPage() {
   return (
-    <div className="w-full my-20">
-      <HeroTitleAndDescription />
-      <HeroButton />
-      <div className="w-full flex flex-col md:flex-row flex-wrap md:flex-nowrap justify-center items-center gap-12 md:gap-20 mt-10">
-        {HeroFooterInfo.map((info, index) => (
-          <HeroFooter key={index} title={info.title} subtitle={info.subtitle} />
-        ))}
+    <div
+      className="w-full min-h-screen bg-cover bg-center relative"
+      style={{ backgroundImage: "url('/herosection.jpeg')" }}
+    >
+      <div className="absolute inset-0 bg-black opacity-60 z-0 backdrop-blur-md"></div>
+      <div className="relative z-10">
+        <HeroTitleAndDescription />
+        <HeroButton />
+        <div className="w-full flex flex-col md:flex-row flex-wrap md:flex-nowrap justify-center items-center gap-12 md:gap-20 mt-10">
+          {HeroFooterInfo.map((info, index) => (
+            <HeroFooter
+              key={index}
+              title={info.title}
+              subtitle={info.subtitle}
+            />
+          ))}
+        </div>
       </div>
     </div>
   );
--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -4,22 +4,34 @@ import Link from "next/link";
 
 export default function Navbar() {
   return (
-    <nav className="flex items-center justify-between bg-transparent p-8 font-sans">
-      <div className="flex items-center space-x-4">
-        <ChartColumn className="bg-gradient-to-br from-green-400 [0%] via-blue-500 [60%] to-purple-500 [70%] text-black w-12 h-12 p-1.5 rounded-4xl" />
+    <nav className="flex flex-col md:flex-row items-center justify-between bg-transparent font-sans border-b shadow-b-lg mb-6 px-6">
+      <div className="flex items-center space-x-6 md:space-x-4">
+        <ChartColumn className="bg-gradient-to-br from-chart-2 [0%] via-chart-1 [60%] to-chart-4 [70%] text-black w-12 h-12 p-1.5 rounded-4xl" />
         <span className="font-bold text-xl">SentioPulse</span>
       </div>
-      <div className="flex space-x-8 ">
-        <Link href="/features" className="text-lg text-muted-foreground hover:text-foreground">
+      <div className="flex space-x-10 md:space-x-8 space-y-6 mt-6">
+        <Link
+          href="/features"
+          className="text-lg text-muted-foreground hover:text-foreground"
+        >
           Features
         </Link>
-        <Link href="/Pricing" className="text-lg text-muted-foreground hover:text-foreground">
+        <Link
+          href="/Pricing"
+          className="text-lg text-muted-foreground hover:text-foreground"
+        >
           Pricing
         </Link>
-        <Link href="/About" className="text-lg text-muted-foreground hover:text-foreground">
+        <Link
+          href="/About"
+          className="text-lg text-muted-foreground hover:text-foreground"
+        >
           About
         </Link>
-        <Link href="Contact" className="text-lg text-muted-foreground hover:text-foreground">
+        <Link
+          href="Contact"
+          className="text-lg text-muted-foreground hover:text-foreground"
+        >
           Contact
         </Link>
       </div>
@@ -28,7 +40,7 @@ export default function Navbar() {
         <Button className="bg-transparent text-foreground hover:bg-muted-foreground text-md">
           Sign in
         </Button>
-        <Button className="font-bold bg-gradient-to-br from-green-400 via-blue-500 to-purple-500 hover:from-green-500 hover:via-blue-600 hover:to-purple-600 text-md">
+        <Button className="font-bold bg-gradient-to-br from-chart-2 via-chart-1 to-chart-4 hover:from-chart-2 hover:via-chart-1 hover:to-chart-4 text-md">
           Get Started
         </Button>
       </div>

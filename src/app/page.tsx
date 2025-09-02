929c1274dd41061ca4ea0cc2c8b65e0259d6be2f
Kalida Tony <kalidatony@gmail.com>
2025-09-03 01:51:38 +0600

    Enhance PricingSection component with updated styles and spacing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,14 +3,15 @@ import StatsSection from "@/components/Stats/StatsSection";
 import MeetSentioPulseSection from "@/components/meetSentioPulseSection";
 import HeroFrontPage from "./hero/page";
 import FeatureFrontPage from "./features/page";
+import PricingSection from "@/components/PricingSection";
 
 export default function HomePage(){
   return <main className="w-full">
     <HeroFrontPage/>
     <FeatureFrontPage/>
     <MeetSentioPulseSection />
+    <PricingSection />
     <StatsSection />
-
     <Footer />
   </main>
 }

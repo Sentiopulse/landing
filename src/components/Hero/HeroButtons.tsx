82c475b1d7f9202d3f85fa2e689641a57549a559
Kalida Tony <kalidatony@gmail.com>
2025-10-08 21:54:18 +0000

    Refactor HeroButtons to use shared button component

--- src/components/Hero/HeroButtons.tsx
+++ src/components/Hero/HeroButtons.tsx
@@ -1,10 +1,11 @@
 "use client";
+import React from "react";
 import { Button } from "../ui/button";
 import { ArrowRight } from "lucide-react";
 import { TrendingUp } from "lucide-react";
 import Link from "next/link";
 
-export default function HeroButton() {
+const HeroButton: React.FC = () => {
   return (
     <div className="flex flex-col md:flex-row justify-center items-center gap-6 mt-6 font-sans">
       <Button
@@ -28,4 +29,6 @@ export default function HeroButton() {
       </Button>
     </div>
   );
-}
+};
+
+export default HeroButton;
